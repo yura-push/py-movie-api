648d4a9ae25274110385d7b94494d698f72d4bf3
Yura Push <youdeimos1@gmail.com>
2025-01-15 19:50:30 +0200

    create movie list and detail function based views

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+def movie_list(request):
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return JsonResponse(serializer.data, safe=False, status=200)
+
+
+def movie_detail(request, pk):
+    if request.method == "GET":
+        movie =get_object_or_404(Movie, pk=pk)
+        serializer = MovieSerializer(movie)
+        return JsonResponse(serializer.data, status=200)
