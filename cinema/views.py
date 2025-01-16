785a360ec570408960388eafc31d938203089b92
Yura Push <youdeimos1@gmail.com>
2025-01-16 13:55:41 +0200

    add API views for POST, PUT, DELETE endpoints

--- cinema/views.py
+++ cinema/views.py
@@ -1,19 +1,36 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
+@api_view(["GET", "POST"])
 def movie_list(request):
     if request.method == "GET":
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return JsonResponse(serializer.data, safe=False, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+@api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
     if request.method == "GET":
-        movie =get_object_or_404(Movie, pk=pk)
         serializer = MovieSerializer(movie)
-        return JsonResponse(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PUT":
+        serializer = MovieSerializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
